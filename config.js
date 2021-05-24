20a5f9db82f20b759f9678e7156e8b72d1e1f7ed
Chikaysci <kjabellar@gmail.com>
2021-05-25 07:08:20 +0800

    Initial lead template file

---
+++ config.js
@@ -0,0 +1,279 @@
+var Configs = {
+  widget_id: 356,
+  domain: "sample.com",
+  domainid: 3443,
+  memberid: 7,
+  title: "Welcome to javapoint.com",
+  logo: "https://cdn.vnoc.com/logos/logo-javapoint1.png",
+  description: "Join our exclusive community of like minded people on javapoint.com",
+  account_ga: "UA-41926954-46",
+  piwik_id: 3,
+  background_image: "https://cdn.vnoc.com/background/tech4.jpg",
+  top_description: "",
+  keywords: "java, point",
+  forsale: "",
+  forsaledefault: "",
+  forsaletext: "This domain belongs to the Global Ventures network. We have interesting opportunities for work, sponsors and partnerships.",
+  additional_html: "PGJyPiA8YSBocmVmPSJodHRwczovL2dvby5nbC9SNFU4c0giIHRhcmdldD0iX2JsYW5rIj48aW1nIHN0eWxlPSJib3JkZXI6MHB4O2hlaWdodDogNjVweCAhaW1wb3J0YW50O3dpZHRoOiA7d2lkdGg6IDIyNXB4ICFpbXBvcnRhbnQ7IiBzcmM9Imh0dHA6Ly9jZG4udm5vYy5jb20vYmFubmVyL2Jhbm5lci1jdGItZWFybi1jdGItdG9rZW5zLnBuZyIgYWx0PSJDcnlwdG8gQ29udHJpYiIgdGl0bGU9IkNyeXB0byBDb250cmliIj48L2E+PGxpbmsgcmVsPSJzdHlsZXNoZWV0IiB0eXBlPSJ0ZXh0L2NzcyIgaHJlZj0iaHR0cDovL3Rvb2xzLmNvbnRyaWIuY29tL2Nzcy9qcXVlcnkuZ2x1ZS5jc3MiPjxzY3JpcHQgc3JjPSJodHRwOi8vdG9vbHMuY29udHJpYi5jb20vanMvanF1ZXJ5LmdsdWUubWluLmpzIj48L3NjcmlwdD48c2NyaXB0IHNyYz0iaHR0cDovL3Rvb2xzLmNvbnRyaWIuY29tL2pzL2dsdWUuanMiPjwvc2NyaXB0PjxkaXYgaWQ9ImJlZm9yZXlvdWdvIiBzdHlsZT0iZGlzcGxheTpub25lOyIgY2xhc3M9ImdsdWVfcG9wdXAgZ2x1ZV9jb250YWluZXIiPjxkaXYgY2xhc3M9ImdsdWVfY2xvc2UiIG9uY2xpY2s9IiQuZ2x1ZV9jbG9zZSgpIj5YPC9kaXY+PGRpdiBjbGFzcz0iZ2x1ZV9jb250ZW50Ij48ZGl2IGNsYXNzPSJ3cmFwLWV4aXQtY29udGVudCB0ZXh0LWNlbnRlciI+PGltZyBjbGFzcz0ibG9nby1leGl0LWN0YiIgc3JjPSJodHRwczovL3MzLmFtYXpvbmF3cy5jb20vYXNzZXRzLnppcHNpdGUubmV0L2ltYWdlcy9qYXlzb24vaWNvbnMvY3VycmVuY3ktY3RiLTQucG5nIiBhbHQ9IiI+PGgxIGNsYXNzPSJiZy10dGxlLWV4aXQiPiBIZWxsbyE8L2gxPjxwPiBXZSBpbnZpdGUgeW91IHRvIG91ciBGaXJzdCBNb3ZlcnMgT3Bwb3J0dW5pdHkgd2l0aCBvdXIgPGJyPkNUQiBjcnlwdG8gdG9rZW4gc2FsZSBzdGFydGluZyA8YiBjbGFzcz0idGV4dC1kYW5nZXIiPk9jdG9iZXIgMTIsIDIwMTc8L2I+Ljxicj5BbWF6aW5nIE9wcG9ydHVuaXRpZXMgd2l0aCB3b3JsZCBsZWFkaW5nIGRpZ2l0YWwgYXNzZXRzIGxpa2UgU3RyZWFtaW5nLmNvbSwgQXBwbGljYXRpb25zLmNvbSBhbmQgb3RoZXJzLiBMZWFybiBtb3JlIGFuZCBnZXQgdXBkYXRlcy48YnI+PC9wPjxwPiA8YSBocmVmPSJodHRwczovL2dvby5nbC9tRXYxM3YiIGNsYXNzPSJidG4gYnRuLWxnIGJ0bi13YXJuaW5nIiB0YXJnZXQ9Il9ibGFuayI+IDxpIGNsYXNzPSJmYSBmYS1jaGVjayI+PC9pPiBHZXQgU3RhcnRlZDwvYT48L3A+PHA+IDxhIGhyZWY9ImphdmFzY3JpcHQ6OyIgb25jbGljaz0iJC5nbHVlX2Nsb3NlKCkiIGNsYXNzPSJoZWxwLWJsb2NrIj4gPHNtYWxsPk5vLCB0aGFua3MgSSBhbSBub3QgaW50ZXJlc3RlZCB0byBiZSBwYXJ0IG93bmVyLjwvc21hbGw+PC9hPjwvcD48L2Rpdj48L2Rpdj48L2Rpdj4=",
+  footer_html: "",
+  footer_banner: "",
+  domain_affiliate_link: "https://referrals.contrib.com/idevaffiliate.php?id=15959&url=https://www.contrib.com/signup/firststep?domain=javapoint.com",
+  data_widget_users: {
+	success: true,
+	data:[
+		{
+			Username:'gauravmeht',
+			FirstName:'gaurav',
+			country:'India'
+		},{
+			Username:'timothytim',
+			FirstName:'Timothy',
+			country:'Marshall Islands'
+		},{
+			Username:'seanclover',
+			FirstName:'Sean',
+			country:'United States'
+		},{
+			Username:'marlincull',
+			FirstName:'Marlin',
+			country:'United States'
+		},{
+			Username:'aliheydarb',
+			FirstName:'Ali',
+			country:'Iran'
+		},{
+			Username:'azrieldavi',
+			FirstName:'Azriel',
+			country:'Guam'
+		}
+	]
+  },
+  data_widget_jobs:{
+	success: true,
+	data:[
+		{
+			domain_name:'ecoemployment.com',
+			job_id:124513,
+			title:'Ecoemployment.com Co-founder'
+		},{
+			domain_name:'tampazoo.com',
+			job_id:124508,
+			title:'Tampazoo.com Co-founder'
+		},{
+			domain_name:'hempdiapers.com',
+			job_id:124503,
+			title:'Hempdiapers.com Co-founder'
+		},{
+			domain_name:'realtydao.org',
+			job_id:124495,
+			title:'Realtydao.org Co-founder'
+		}
+	]
+  },
+  related_domains:[
+	{
+		domain_name:'coderep.com'
+	},{
+		domain_name:'appschallenge.com'
+	},{
+		domain_name:'appserver.com'
+	},{
+		domain_name:'venturescript.com'
+	},{
+		domain_name:'javasolution.com'
+	},{
+		domain_name:'directorybot.com'
+	},{
+		domain_name:'accesscode.com'
+	},{
+		domain_name:'bottleservices.com'
+	},{
+		domain_name:'cleantechmanagement.com'
+	},{
+		domain_name:'nanotechnology.com'
+	},{
+		domain_name:'robotcentre.com'
+	},{
+		domain_name:'technologypanel.com'
+	},
+  ],
+  vertical_domains:[
+	{
+		domain_name:'javaengineers.com',
+		domain_id:25426,
+		logo:'https://cdn.vnoc.com/logos/logo-JavaEngineers1.png',
+		title:'Welcome to javaengineers.com',
+		slug:'technology'
+	},{
+		domain_name:'appschallenge.com',
+		domain_id:11687,
+		logo:'https://cdn.vnoc.com/logos/logo-appschallenge1.png',
+		title:'Appschallenge.com',
+		slug:'technology'
+	},{
+		domain_name:'applications.net',
+		domain_id:1838,
+		logo:'https://cdn.vnoc.com/logos/logo-Applications-4.png',
+		title:'Welcome to applications.net',
+		slug:'technology'
+	},{
+		domain_name:'storebot.com',
+		domain_id:14031,
+		logo:'https://cdn.vnoc.com/logos/logo-Storebot1.png',
+		title:'Welcome to storebot.com',
+		slug:'technology'
+	},{
+		domain_name:'codebuilder.com',
+		domain_id:17067,
+		logo:'https://cdn.vnoc.com/logos/logo-codebuilder1.png',
+		title:'Welcome to Codebuilder.com',
+		slug:'technology'
+	},
+  ],
+  vertical_domains_nologo:[
+	{
+		domain_name:'inventionspecialist.com',
+		domain_id:19051,
+		logo:'',
+		title:'Inventionspecialist.com',
+		slug:'technology'
+	},{
+		domain_name:'bythebottle.com',
+		domain_id:5321,
+		logo:'',
+		title:'Welcome to bythebottle.com',
+		slug:'technology'
+	},{
+		domain_name:'applereservations.com',
+		domain_id:20034,
+		logo:'',
+		title:'Welcome to applereservations.com',
+		slug:'technology'
+	},{
+		domain_name:'codecaster.com',
+		domain_id:7184,
+		logo:'',
+		title:'Welcome to codecaster.com',
+		slug:'technology'
+	},{
+		domain_name:'javabanker.com',
+		domain_id:12257,
+		logo:'',
+		title:'Welcome to javabanker.com',
+		slug:'technology'
+	},
+  ],
+  fundcampaigns:[],
+  rolesarray:[
+	{
+		role_id:24,
+		role_name:'Advisor',
+	},{
+		role_id:43,
+		role_name:'CEO',
+	},{
+		role_id:34,
+		role_name:'Chief Technology Officer',
+	},{
+		role_id:30,
+		role_name:'Co-founder',
+	},{
+		role_id:35,
+		role_name:'Community Manager',
+	},{
+		role_id:7,
+		role_name:'Content Manager',
+	},
+  ],
+  countriesarray:[
+	  {
+		country_id:11,
+		name:'Argentina',
+	  },{
+		country_id:12,
+		name:'Armenia',
+	  },{
+		country_id:14,
+		name:'Australia',
+	  },
+  ],
+  industriesarray:[
+	{
+		IndustryId:6,
+		Name:'Admin Support'
+	},{
+		IndustryId:12,
+		Name:'Autos'
+	},{
+		IndustryId:14,
+		Name:'Business Services'
+	},{
+		IndustryId:19,
+		Name:'Computer and Technology'
+	},{
+		IndustryId:5,
+		Name:'Design & Multimedia'
+	},{
+		IndustryId:17,
+		Name:'Education'
+	}
+  ],
+  parnershiptypes:['Sponsorship Marketing Partnerships','Distribution Marketing Partnerships','Affiliate Marketing Partnerships','Added Value Marketing Partnerships'],
+  featuredsites:[
+	  {
+		domain_name:'dailymed.com',
+		logo:'',
+		description:'Join our exclusive community of like minded people on dailymed.com',
+		title:'Welcome to dailymed.com'
+	  },
+	  {
+		domain_name:'handyman.com',
+		logo:'https://cdn.vnoc.com/logos/logo-handyman.png',
+		description:'Join our exclusive community of like minded people on handyman.com',
+		title:'Handyman'
+	  },
+	  {
+		domain_name:'mychallenge.com',
+		logo:'https://cdn.vnoc.com/logos/logo-mychallenge5.png',
+		description:'Join our exclusive community of like minded people on mychallenge.com',
+		title:'Welcome to mychallenge.com'
+	  },
+	  {
+		domain_name:'virtualinterns.com',
+		logo:'https://cdn.vnoc.com/logos/logo-virtualinterns.png',
+		description:'Join our exclusive community of like minded people on virtualinterns.com',
+		title:'Virtual Interns'
+	  }
+  ],
+  topsites:[
+	  {
+		domain_name:'dailymed.com',
+		logo:'https://cdn.vnoc.com/logos/logo-DailyMed1.png',
+		description:'Join our exclusive community of like minded people on dailymed.com',
+		title:'Welcome to dailymed.com'
+	  },
+	  {
+		domain_name:'handyman.com',
+		logo:'https://cdn.vnoc.com/logos/logo-handyman.png',
+		description:'Join our exclusive community of like minded people on handyman.com',
+		title:'Handyman'
+	  },
+	  {
+		domain_name:'mychallenge.com',
+		logo:'https://cdn.vnoc.com/logos/logo-mychallenge5.png',
+		description:'Join our exclusive community of like minded people on mychallenge.com',
+		title:'Welcome to mychallenge.com'
+	  },
+	  {
+		domain_name:'virtualinterns.com',
+		logo:'https://cdn.vnoc.com/logos/logo-virtualinterns.png',
+		description:'Join our exclusive community of like minded people on virtualinterns.com',
+		title:'Virtual Interns'
+	  }
+  ],
+  social_fb:'https://www.facebook.com/pages/Javapoint/646287392104713',
+  social_gplus:'https://plus.google.com/u/0/107137956951125091896/',
+  social_twitter:'https://twitter.com/contrib',
+  social_linkedin:'http://www.linkedin.com/company/javapoint',
+  social_gtube:'https://www.youtube.com/user/javapointcom'
+}
