43339be3f9229ddcadc82c236eaeec0d73b0a070
Chikaysci <kjabellar@gmail.com>
2021-05-25 07:08:27 +0800

    Initial lead template file

---
+++ precache-manifest.a54554c84f4e8ddc9db4febc1c1d3636.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce4c9931d8c7c312790109decf109118",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1d5b8e9191a73a5e421",
+    "url": "/static/css/2.c3594674.chunk.css"
+  },
+  {
+    "revision": "9832e9f6db36f60031b8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f1d5b8e9191a73a5e421",
+    "url": "/static/js/2.6e606fd2.chunk.js"
+  },
+  {
+    "revision": "a569361d31625f49e7e30aea21628ed5",
+    "url": "/static/js/2.6e606fd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9832e9f6db36f60031b8",
+    "url": "/static/js/main.fcd9d49b.chunk.js"
+  },
+  {
+    "revision": "42f678b35c1bf6851feb",
+    "url": "/static/js/runtime-main.d2d22ceb.js"
+  }
+]);
